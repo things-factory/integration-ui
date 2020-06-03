9fe356b1400bb63e6010cb2fe759c6747be8f3be
Murky <moosung.gil@hatiolab.com>
2020-06-03 16:00:04 +0900

    separate node-red-integration

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,12 +1,7 @@
 import '@material/mwc-icon'
 import { registerEditor } from '@things-factory/grist-ui'
-import { SET_INTEGRATION_MODELLER_SETTINGS } from '@things-factory/integration-base'
 import { ADD_MODELLER_EDITORS } from '@things-factory/modeller-ui'
-import { ADD_MORENDA } from '@things-factory/more-base'
-import { ADD_SETTING } from '@things-factory/setting-base'
-import { client, store } from '@things-factory/shell'
-import gql from 'graphql-tag'
-import { html } from 'lit-html'
+import { store } from '@things-factory/shell'
 import './editors/entity-editor'
 import './editors/property-editor'
 import { ConnectionSelector } from './grist/connection-selector'
@@ -16,7 +11,6 @@ import { JsonGristEditor } from './grist/json-grist-editor'
 import { ParametersEditor } from './grist/parameters-editor'
 import { TaskTypeSelector } from './grist/task-type-selector'
 import './viewparts/modeller-url-setting-let'
-import { INTEGRATION_MODELLER_SETTING_CATEGORY } from './viewparts/modeller-url-setting-let'
 
 export default function bootstrap() {
   registerEditor('task-type', TaskTypeSelector)
@@ -35,65 +29,4 @@ export default function bootstrap() {
       'entity-selector': 'property-editor-entity-selector'
     }
   })
-
-  store.dispatch({
-    type: ADD_SETTING,
-    setting: {
-      seq: 30,
-      template: html` <integration-modeller-setting-let></integration-modeller-setting-let> `
-    }
-  })
-
-  store.dispatch({
-    type: ADD_MORENDA,
-    morenda: {
-      icon: html` <mwc-icon>account_tree</mwc-icon> `,
-      name: html` <i18n-msg msgid="text.modeller"></i18n-msg> `,
-      position: 'REAR_END',
-      action: async () => {
-        window.open('/integration/modeller', 'integration_modeller')
-      }
-    }
-  })
-
-  getIntegrationModellerSettings().then(settings => {
-    var settingData = {}
-
-    settings.forEach(setting => {
-      settingData[setting.name.replace(/integration.modeller\-(\w+)/, '$1')] = setting.value
-    })
-
-    store.dispatch({
-      type: SET_INTEGRATION_MODELLER_SETTINGS,
-      ...settingData
-    })
-  })
-}
-
-async function getIntegrationModellerSettings() {
-  const response = await client.query({
-    query: gql`
-      query($filters: [Filter], $pagination: Pagination, $sortings: [Sorting]) {
-        responses: settings(filters: $filters, pagination: $pagination, sortings: $sortings) {
-          items {
-            name
-            value
-          }
-        }
-      }
-    `,
-    variables: {
-      filters: [
-        {
-          name: 'category',
-          operator: 'eq',
-          value: INTEGRATION_MODELLER_SETTING_CATEGORY
-        }
-      ],
-      pagination: { page: 1, limit: 100 },
-      sortings: []
-    }
-  })
-
-  return response?.data?.responses?.items
 }
